4c426297ea03fac3f1b7a69acb5452f07bc06645
VishwaB <viratvishwa93@gmail.com>
2023-02-13 17:27:04 +0530

    add of two nums

--- TestProject/src/Sumprg.java
+++ TestProject/src/Sumprg.java
@@ -1,4 +1,24 @@
 
 public class Sumprg {
 
-}
+		static int sum(int n)
+		{
+			int sum = 0;
+			for(int i=1; i<=n; i++)
+			{
+				sum = sum+i;
+			}
+			return sum;
+		}
+		
+
+		public static void main(String[] args) {
+			int x = sum(8);
+	        System.out.println("Sum ="+x);
+	        
+	    	int a = 5;
+			int b = 3;
+			int c = 1;			
+		}
+
+	}
