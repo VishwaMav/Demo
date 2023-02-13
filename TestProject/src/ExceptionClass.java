09df59c5b5bdacaeb03b35af6b1a5e26012a8abf
VishwaB <viratvishwa93@gmail.com>
2023-02-13 16:21:32 +0530

    added ExceptionClass.java

---
+++ TestProject/src/ExceptionClass.java
@@ -0,0 +1,29 @@
+
+public class ExceptionClass {
+
+		public static void main(String[] args) 
+		{
+			int a = 4;
+			int b = 0;
+			int c = 0;
+			
+			try
+			{	
+				c = a/b;
+				System.out.println(c);
+			}
+			
+			catch(Exception e)
+			{
+				System.out.println("Need to change a value");
+			}
+			
+			finally
+			{
+				System.out.println("Log off");
+			}
+		}
+
+	}
+
+
